6fb6c0c6ea1d311fdee097dbb89f666ab2c62e3f
latexers <latexers@gmail.com>
2016-01-08 01:05:23 +0800

    cvFoundations

--- src/CVFoundations.py
+++ src/CVFoundations.py
@@ -1,6 +1,8 @@
 #encoding=utf-8
 __author__ = 'Richard'
 import urllib2
+from bs4 import BeautifulSoup
+import re
 
 class PaperSnifer():
 
@@ -10,17 +12,29 @@ class PaperSnifer():
         self.biburls = []
         self.url = url
         self.papertitles = []
+        self.contents = ''
 
-    def getContents(self, url):
-        page = urllib2.urlopen(url)
+    def getContents(self):
+        page = urllib2.urlopen(self.url)
         contents = page.read()
         return contents
 
-    def getInfo(self, contents):
+    def getInfo(self):
+        soup = BeautifulSoup(contents)
+        print soup.title
+        urllist = soup.find_all(href=re.compile("pdf"))
+        titleList = soup.find_all('div')
+        print urllist
+        print titleList
 
-        return
+        return 0
+
     def downloader(self, pdflinks):
       return 0
 
 
+if __name__ == '__main__':
+    paperSnifer = PaperSnifer("http://www.cv-foundation.org/openaccess/ICCV2015.py")
+    contents = paperSnifer.getContents()
+    paperSnifer.getInfo()
 
